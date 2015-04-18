2f83991d4bc80d912655368b0910a3b2ec505edb
rlstober <bobstober@gmail.com>
2015-04-18 12:53:27 -0400

    Added reset UI

--- server.R
+++ server.R
@@ -8,27 +8,44 @@
 library(shiny)
 default<-c("Excellent","Wonderful","Marvelous", "Intuitive","Amusing")
 load("./data/englishStop.RData")
-deftxt<-"This application is"
+load("./data/uniTDMfreq.RData")
+deftin<-"This application is"
+defnw<-3
   
   shinyServer(function(input, output) {
    
-  output$nw<-renderPrint({input$nw})
-  output$tin<-renderPrint({input$tin})
-#   if (length(input$tin) == 0 )
-#     output$tin= renderPrint({deftxt})
-#     else output$tin<-renderPrint({input$tin})
   
-  #g1<-sample(default,1)
-  #output$predict <- renderPrint({sample(default,1)})
-#   output$predict <- renderTable({
-#     output$nw<-renderPrint(input$nw)
-#     output$tin<-renderPrint(input$tin)
-#     #
-#     g1<-as.matrix(sample(default,input$nw-1),dimanes = list(NULL,"Words"))
-#     # add stop word
-#     g1<-rbind(g1,(sample(englishStop,1)))
-
+  
+  output$resetable_input <- renderUI({
+    times <- input$reset_input
+    list(
+      h4('The application will attempt to predict the next word to be typed'),
+      br(),
+      
+      radioButtons("nw", label = h4("Set the Number of Single Word Predictions"),
+                   choices = list("One" = 1, "Three" = 3,
+                                  "Five" = 5),selected = 3),  
+      textInput('tin',
+                label=h4('Enter an incomplete phrase below'),
+                value = "This application is"),
+      h4('Hit the Predict button to update the prediction or Reset to start over')
+    ) # end list
   })
-    
- # })
+  
+  output$nw<-renderPrint({input$nw})
+  #output$tin<-renderPrint({input$tin})
+  output$tin<-reactive({
+   if (length(input$tin) && sub(" ","",input$tin) == "" ) deftin else input$tin    
+  })
+  
+  
+  output$predict <- renderTable({
+    if(is.null(input$nw)==TRUE) sz<-defnw else sz<-input$nw
+    g1<-as.matrix(sample(default,sz),dimanes = list(NULL,"Words"))
+    # add stop word
+    #if (sz!=1) g1<-rbind(g1,(sample(englishStop,1)))
+  })
+  
+  
+  })
 
--- ui.R
+++ ui.R
@@ -15,18 +15,19 @@ shinyUI(fluidPage(
   
   # Sidebar with a slider input for number of bins
   sidebarPanel(
-    h4('The application will attempt to predict the next word to be typed'),
-    br(),
-    
-    radioButtons("nw", label = h4("Set the Number of Single Word Predictions"),
-                 choices = list("One" = 1, "Three" = 3,
-                             "Five" = 5),selected = 5),  
-    textInput('tin',
-                label=h4('Enter an incomplete phrase below'),
-                value = "This application is"),
-    h4('Hit the Predict button to update the prediction or Reset to start over'), 
-    actionButton("predict", label = "Predict", icon = icon("question",  lib = "font-awesome")),
-    actionButton("reset", label = "Reset", icon = icon("refresh",  lib = "font-awesome")),
+    uiOutput('resetable_input'),
+#     h4('The application will attempt to predict the next word to be typed'),
+#     br(),
+#     
+#     radioButtons("nw", label = h4("Set the Number of Single Word Predictions"),
+#                  choices = list("One" = 1, "Three" = 3,
+#                              "Five" = 5),selected = 3),  
+#     textInput('tin',
+#                 label=h4('Enter an incomplete phrase below'),
+#                 value = "This application is"),
+#     h4('Hit the Predict button to update the prediction or Reset to start over'), 
+    actionButton("predictButton", label = "Predict", icon = icon("question",  lib = "font-awesome")),
+    actionButton("reset_input", label = "Reset", icon = icon("refresh",  lib = "font-awesome")),
     #submitButton(text = "Make Prediciton"),
    br(),
    h4('This application was built with Rstudio and Shiny'),
@@ -38,20 +39,25 @@ shinyUI(fluidPage(
   
   # Show a plot of the generated distribution
   mainPanel(
-    h4('This application demonstrates text prediction'),
-    h4('Text prediction is an example of Natural Language processing or NLP'),
-    br(),   
-    helpText("This application demonstrates text predicition."),
-    
-    
-    h4('The number of words to predict is'),
-    verbatimTextOutput("nw"),
-    h4('Your incomplete phrase is'),
-    verbatimTextOutput("tin"),
-    br(),
-    h4('My guess is '),
-    #verbatimTextOutput("predict")   
-    tableOutput("predict")  
-
+    tabsetPanel(type = "tabs", 
+      tabPanel("Prediction",
+        h4('This application demonstrates text prediction'),
+        h4('Text prediction is an example of Natural Language processing or NLP'),
+        br(),   
+        h4('The number of words to predict is'),
+        verbatimTextOutput("nw"),
+        h4('Your incomplete phrase is'),
+        verbatimTextOutput("tin"),
+        br(),
+        h4('My guess is '),
+        #verbatimTextOutput("predict")   
+        tableOutput("predict") 
+        ), # end predict tab
+      tabPanel("Help",
+               h4('This application demonstrates text prediction'),
+               h4('Text prediction is an example of Natural Language processing or NLP'),
+               br()   
+               ) #end help tab
+      ) # end tab panel
   )
 ))
